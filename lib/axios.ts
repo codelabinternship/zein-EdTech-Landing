1cd08ad681796d6c6a8ba972e2e5eac08e3de7b1
Abdurakhmonov Sharif <sharifabduraxmonov@mail.ru>
2025-05-22 23:32:03 +0500

    update baseURL

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL, // like http://localhost:8000/api
+  baseURL: "https://zein-edtech-server.onrender.com" // like http://localhost:8000/api
 })
 
 export default api
