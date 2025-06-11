053891efd8cc91a811d884d7a31b378b6074501b
Abdurakhmonov Sharif <sharifabduraxmonov@mail.ru>
2025-06-11 18:18:26 +0500

    handle seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,18 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" />
          <meta name="google-site-verification" content="eUJyGjZpPbGoAnABZtokjQLdCNPJfWbSwtUYNDmXoHs" />
          <meta name="yandex-verification" content="79c50c53d0062032" />
+           <title>
+          ZEIN EDTECH | Til Kurslari – Ingliz, Rus, Arab, Turk, Koreys
+          Tillarini Onlayn O‘rganing
+        </title>
+        <meta
+          name="description"
+          content="ZEIN EDTECH — 2013 yildan buyon til o‘rganishni qulay va samarali qiluvchi o‘quv markazi. Ingliz, Rus, Arab, Turk, Koreys tillari bo‘yicha online kurslar, tajribali o‘qituvchilar, moslashuvchan dars jadvali va sertifikat beriladi."
+        />
+        <meta
+          name="keywords"
+          content="ingliz tili kurslari, rus tili kurslari, turk tili kurslari, arab tili kurslari, koreys tili kurslari, online ingliz tili o‘rganish, onlayn rus tili saboqlari, koreyscha o‘rganish onlayn"
+        />
       </head>
       <body>
         <ClientLayout>{children}</ClientLayout>
--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: "https://zein-edtech-server.onrender.com" // like http://localhost:8000/api
+  baseURL: "https://zein-edtech-server.onrender.com" 
+  // baseURL: "http://localhost:8000",
 })
 
 export default api
